fa2572a05e5dcd3c39e22ea608f9e76f2981f798
Olivier Mengué <dolmen@cpan.org>
2011-09-29 19:42:48 +0200

    More use cases to test

---
+++ t/lib/ExportsOK.pm
@@ -0,0 +1,11 @@
+package t::lib::ExportsMissing;
+
+use Exporter::Lazy;
+
+@EXPORT_LAZY = 'toto';
+
+sub toto
+{
+}
+
+1;
---
+++ t/lib/SubMissing.pm
@@ -0,0 +1,14 @@
+package t::lib::ExportsMissing;
+
+use strict;
+use warnings;
+
+use Exporter::Lazy;
+
+our @EXPORT_LAZY = 'titi';
+
+sub toto
+{
+}
+
+1;
